1ea498b01ead3eac37f2d77dcae9fe57fbae1334
Amirreza <amirreza.m13825@gmail.com>
2023-08-23 18:56:44 +0330

    develop the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <header className="flex justify-between mb-4 items-center">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href={"/new"}
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4"></ul>
+    </>
+  );
 }
